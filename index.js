fd716f17a38e57bb020dc3d5dde035f8cd1bba80
thivviyan98 <thivviyan1998@gmail.com>
2023-10-22 15:45:50 +0530

    first shit

---
+++ index.js
@@ -0,0 +1,47 @@
+// var a = 12;
+// var a = 21;
+// console.log(a);
+
+// let a = 12;
+// let a = 21;
+// console.log(a);
+
+// let num2;
+// for (let i = 0; i < 3; i++) {
+//   if (i == 2) {
+//     num2 = function () {
+//       return i;
+//     };
+//   }
+// }
+
+// console.log(num2());
+
+// const name = "Ram";
+// name = "sri";
+
+//const cannnot be mutate with variable values
+
+const s = [5, 7, 2];
+s = [3, 5, 6];
+
+// s.pop();
+// s.pop();
+// s.push(7);
+// s.unshift(2);
+
+// console.log(s);
+
+// const myObj = {
+//     [1,2,34,4],
+//     [3,5,78,8]
+//     [1,5,78,9]
+// }
+
+// const s = [
+//   { name: "sri", age: 24 },
+//   {
+//     name: "ram",
+//     age: 12,
+//   },
+// ];
---
+++ prevent.js
@@ -0,0 +1,30 @@
+// const myObj = {
+//   name: "thanu",
+//   age: "24",
+// };
+
+// console.log(myObj); // thanu 24
+
+// // Object.freeze(myObj); // mutation breech
+
+// // myObj.name = "Shan";
+// // myObj.age = 25;
+
+// console.log(myObj); //
+
+// const two = () => "Thanu";
+// console.log(two());
+
+// const two2 = myFun(){
+//     const b = 2;
+//     return b
+// }
+
+// const name = () => {
+//   setInterval(() => {
+//     console.log("sairam");
+//   }, 1000);
+// };
+
+// console.log(name())
+console.log("object");
